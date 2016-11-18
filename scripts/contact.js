23aee91864741fc772d68b1a8038bdb1e6a68334
dubKelly <js.neeb1780@gmail.com>
2016-11-17 21:11:32 -0500

    js form validation

--- scripts/contact.js
+++ scripts/contact.js
@@ -4,25 +4,61 @@ var done = document.getElementById("done");
 var part = document.getElementById("part");
 var partIndex = 8;
 var next = 0;
+var verify = false;
 for (var i = 0; i < input.length; i++) {
 	input[i].onkeydown = function(event) {
-		this.style.textAlign = "center";
-		this.style.width = "100%"
 		var key = event.keyCode;
-		var elem = this;
 		//	Avoid confusion with non-tab users
 		if (key === 9 || key === 13) {
 			event.preventDefault();
-			next++;
-			partIndex -= 2;
-			part.style.zIndex = partIndex;
+			if (next === 0 && this.value.length === 0) {
+				if (verify === false) {
+					this.style.width = this.offsetWidth + 15 + "px";
+					this.placeholder = this.placeholder + "*";
+					verify = true;
+				}
+				else {
+					console.log('"Insanity: doing the same thing over and over again and expecting different results." -Albert Einstein');
+				}
+			}
+			else if (next === 1 && this.value.length === 0) {
+				input[2].style.display = "none";
+				next = next + 2;
+				input[next].focus();
+				partIndex = partIndex - 4;
+				part.style.zIndex = partIndex;
+				verify = false;
+				func();
+			}
+			else if (next === 3 && input[1].value.length === 0 && this.value.length === 0) {
+				if (verify === false) {
+					this.style.width = this.offsetWidth + 15 + "px";
+					this.placeholder = this.placeholder + "*";
+					verify = true;
+				}
+				else {
+					console.log('"When you have bacon in your mouth, it doesn\'t matter who\'s president" -Louis C.K.')
+				}
+			}
+			else {
+				next++;
+				partIndex -= 2;
+				part.style.zIndex = partIndex;
+				verify = false;
+				func();
+				if (next === 4) {
+					message.focus();
+				}
+				else {
+					input[next].focus();
+				}
+			}
+			var elem = this;
 			if (next === 4) {
-				message.focus();
 				var nextElem = message;
 			}
 			else {
-				input[next].focus();
-				nextElem = input[next];
+				nextElem = input[next];	
 			}
 			function func() {
 				var pos = 50;
@@ -46,15 +82,34 @@ for (var i = 0; i < input.length; i++) {
 					}
 				}
 			}
-		func();
+		}
+		else {
+			this.style.textAlign = "center";
+			this.style.width = "100%"
 		}
 	}
 }
-message.onkeydown = function() {
-	message.style.maxHeight = "51%";
-	message.style.width = "90%";
-	message.style.textAlign = "center";
-	done.style.display = "inline-block"
+message.onkeydown = function(event) {
+	var key = event.keyCode;
+	if (key === 9 || key === 13) {
+		if (next === 4 && message.value.length === 0) {
+			event.preventDefault();
+			if (verify === false) {
+				message.style.width = message.offsetWidth + 15 + "px";
+				message.placeholder = message.placeholder + "*";
+				verify = true;
+			}
+			else {
+				console.log("Have you ever noticed that you can always see your nose?")
+			}
+		}
+	}
+	else {
+		message.style.maxHeight = "51%";
+		message.style.width = "90%";
+		message.style.textAlign = "center";
+		done.style.display = "inline-block"
+	}
 }
 message.onkeyup = function() {
 	message.style.height = (message.scrollHeight) - 4 + "px";
